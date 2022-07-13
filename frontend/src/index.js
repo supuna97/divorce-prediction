f053067ec92c6017439180e6f71d2e0df2ea0046
supun-na97 <supunnanayakkara01@gmail.com>
2022-07-13 22:22:22 +0530

    Some changes of code formats

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,18 +6,16 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( <
-    React.StrictMode >
-    <
-    App / >
-    <
-    /React.StrictMode>
+root.render(
+    <React.StrictMode>
+    <App />
+    </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
